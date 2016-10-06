8af32e27caab361d3a31091c56b481c36a20bc0e
Shalom Nyende <shalom.nyende@gmail.com>
2016-10-06 15:08:26 +0300

    Config

--- src/EduSys/settings.py
+++ src/EduSys/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'online_status',
+    
 ]
 
 MIDDLEWARE_CLASSES = [
@@ -120,4 +120,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
+
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
+]
+STATIC_ROOT = os.path.join(BASE_DIR, 'static_root')
--- src/EduSys/urls.py
+++ src/EduSys/urls.py
@@ -13,13 +13,12 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.conf.urls import url, include
+from django.conf.urls import url
 from django.contrib import admin
 from accounts import views
 
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^online/', include('online_status.urls')),
     url(r'^home/', views.home, name='home'),
 ]
